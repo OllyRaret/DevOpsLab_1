64bc578afe66438a290d24201b26c46a2af1d810
Ollyraret <olenka.lakeeva12@gmail.com>
2023-06-01 21:59:41 +0300

    game change

--- main.py
+++ main.py
@@ -1,193 +1,270 @@
+import msvcrt
+import os
 import sys
-import pygame
 import random
+import time
+from dataclasses import dataclass, asdict
 
-font_path = pygame.font.match_font('freesansbold')
 
-class Tile:
-    def __init__(self, number, x, y, speed, size):
-        self.number = number
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.size = size
-        self.target = [0, 0]
-        self.direction = 0
-        self.move = False
+def clear():
+    os.system('cls')
 
-    def Update(self):
-        if self.move and (self.x != self.target[0] or self.y != self.target[1]):
-            if self.direction == 0:
-                self.y -= self.speed
-                if self.y <= self.target[1]:
-                    self.y = self.target[1]
-                    self.move = False
-            elif self.direction == 1:
-                self.x -= self.speed
-                if self.x <= self.target[0]:
-                    self.x = self.target[0]
-                    self.move = False
-            elif self.direction == 2:
-                self.x += self.speed
-                if self.x >= self.target[0]:
-                    self.x = self.target[0]
-                    self.move = False
+
+@dataclass
+class Player:
+    symbol: str
+    x: int
+    y: int
+    healthPoints: int
+    scorePoints: int
+
+
+@dataclass
+class Meteor:
+    symbol: str
+    x: int
+    y: int
+    direction: int
+    damagePoints: int
+    speedPoints: int
+
+
+class PlayerHandle:
+    def __init__(self, sym, x, y, hp, sp):
+        self.player = Player(sym, x, y, hp, sp)
+
+    def get_dataclass(self):
+        return asdict(self.player)
+
+    def edit(self, key, value):
+        self.player.__dict__[key] = value
+
+
+class MeteorHandle:
+    def __init__(self, sym, x, y, direct, dp, sp):
+        self.meteor = Meteor(sym, x, y, direct, dp, sp)
+
+    def get_dataclass(self):
+        return asdict(self.meteor)
+
+    def edit(self, key, value):
+        self.meteor.__dict__[key] = value
+
+
+def generateMeteor(n, m):
+    chance = random.randint(0, 3)
+    return MeteorHandle("*",
+                        random.randint(0, n - 1) if chance == 0 else (
+                            0 if chance == 1 else (random.randint(0, n - 1) if chance == 2 else n - 1)),
+                        0 if chance == 0 else (random.randint(0, m - 1) if chance == 1 else (
+                            m - 1 if chance == 2 else random.randint(0, m - 1))),
+                        chance,
+                        random.randint(25, 75),
+                        random.randint(1, 5))
+
+
+def loadLevel(n, m):
+    return [[" " for j in range(n)] for i in range(m)]
+
+
+def updateLevel(mapObj, entityList):
+    for entity in entityList:
+        mapObj[entity.get_dataclass()["y"]][entity.get_dataclass()["x"]] = entity.get_dataclass()["symbol"]
+
+
+def printLevel(mapObj):
+    print(" _" * (len(mapObj[0]) + 1))
+    for i in range(len(mapObj)):
+        print(end="| ")
+        for j in range(len(mapObj[0])):
+            print(mapObj[i][j], end=" ")
+        print(end="|\n")
+    print(" ‾" * (len(mapObj[0]) + 1))
+
+
+def printPlayerInfo(playerObj):
+    print(
+        f"Жизни: {playerObj.get_dataclass()['healthPoints'] if playerObj.get_dataclass()['healthPoints'] > 0 else 'ВЫ ПОГИБЛИ'}\tОчки: {playerObj.get_dataclass()['scorePoints']}")
+
+
+def printRules():
+    print(
+        "\nУправление:\n\tW: ↑\tS: ↓\n\tA: ←\tD: →\t\nПравила:\n\t* - метеориты\tP - игрок\n\nНаберите 100 очков, избегайте метеоритов\t")
+
+
+def damageTaken(mapObj, entity):
+    for i in range(2):
+        clear()
+        print("\033[41m")
+        clear()
+        printInterface(mapObj, entity)
+        time.sleep(0.005)
+
+        clear()
+        print("\033[40m")
+        clear()
+        printInterface(mapObj, entity)
+        time.sleep(0.005)
+
+    entity[-1].edit("symbol", "P")
+
+
+def printInterface(mapObj, entity):
+    clear()
+    printPlayerInfo(entity[-1])
+    printLevel(mapObj)
+    printRules()
+
+
+def collisionCheck(obj, playerObj):
+    if obj.get_dataclass()["x"] == playerObj.get_dataclass()["x"] and \
+            obj.get_dataclass()["y"] == playerObj.get_dataclass()["y"]:
+        playerObj.edit("symbol", "#")
+        playerObj.edit("healthPoints",
+                       playerObj.get_dataclass()["healthPoints"] - obj.get_dataclass()[
+                           "damagePoints"])
+
+
+def changeComplexity():
+    clear()
+    print("Meteors SUCK --- by Finik Aka Moxem\t\n")
+    print("\tВыбор сложности.\t\n")
+    print("1.\tЛегкая.\t")
+    print("2.\tНормальная.\t")
+    print("3.\tСложная.\t")
+
+
+def mainMenu(complex):
+    clear()
+    print("Meteors SUCK --- by Finik Aka Moxem\t\n")
+    print("\tГлавное меню.\t\n")
+    print("1.\tНачать игру.\t")
+    print(f"2.\tИзменить сложность.\t({complex})")
+    print("3.\tВыход.\t")
+
+
+def gameOver(arg, m):
+    if arg:
+        clear()
+        print("\n" * (m // 2) + "\tВЫ ПРОИГРАЛИ!\t" + "\n" * (m // 2) + "Нажмите любую клавишу...")
+        msvcrt.getwch()
+    else:
+        clear()
+        print("\n" * (m // 2) + "\tВЫ ПОБЕДИЛИ!\t" + "\n" * (m // 2) + "Нажмите любую клавишу...")
+        msvcrt.getwch()
+    mainMenu("Легкая")
+
+
+def main(n):
+    m = n
+    player = PlayerHandle("P", n // 2, m // 2, 100, 0)
+    entity = [player]
+
+    while player.get_dataclass()["healthPoints"] > 0 and player.get_dataclass()["scorePoints"] < 100:
+        level = loadLevel(n, m)
+        updateLevel(level, entity)
+        printInterface(level, entity)
+
+        movePlayer = msvcrt.getwch()
+        while movePlayer not in "WASDwasdЦФЫВцфыв":
+            movePlayer = msvcrt.getwch()
+
+        if (movePlayer.lower() == "w" or movePlayer.lower() == "ц") and player.get_dataclass()["y"] > 0:
+            player.edit("y", player.get_dataclass()["y"] - 1)
+            for obj in entity:
+                if isinstance(obj, MeteorHandle):
+                    collisionCheck(obj, player)
+        elif (movePlayer.lower() == "s" or movePlayer.lower() == "ы") and player.get_dataclass()["y"] < m - 1:
+            player.edit("y", player.get_dataclass()["y"] + 1)
+            for obj in entity:
+                if isinstance(obj, MeteorHandle):
+                    collisionCheck(obj, player)
+        elif (movePlayer.lower() == "a" or movePlayer.lower() == "ф") and player.get_dataclass()["x"] > 0:
+            player.edit("x", player.get_dataclass()["x"] - 1)
+            for obj in entity:
+                if isinstance(obj, MeteorHandle):
+                    collisionCheck(obj, player)
+        elif (movePlayer.lower() == "d" or movePlayer.lower() == "в") and player.get_dataclass()["x"] < n - 1:
+            player.edit("x", player.get_dataclass()["x"] + 1)
+            for obj in entity:
+                if isinstance(obj, MeteorHandle):
+                    collisionCheck(obj, player)
+
+        for step in range(1, 6):
+            for obj in entity:
+                if isinstance(obj, MeteorHandle):
+                    if obj.get_dataclass()["direction"] == 0:
+                        if obj.get_dataclass()["y"] < m - 1:
+                            if obj.get_dataclass()["speedPoints"] >= step:
+                                obj.edit("y", obj.get_dataclass()["y"] + 1)
+                        else:
+                            entity.pop(entity.index(obj))
+                    elif obj.get_dataclass()["direction"] == 1:
+                        if obj.get_dataclass()["x"] < n - 1:
+                            if obj.get_dataclass()["speedPoints"] >= step:
+                                obj.edit("x", obj.get_dataclass()["x"] + 1)
+                        else:
+                            entity.pop(entity.index(obj))
+                    elif obj.get_dataclass()["direction"] == 2:
+                        if obj.get_dataclass()["y"] > 0:
+                            if obj.get_dataclass()["speedPoints"] >= step:
+                                obj.edit("y", obj.get_dataclass()["y"] - 1)
+                        else:
+                            entity.pop(entity.index(obj))
+                    elif obj.get_dataclass()["direction"] == 3:
+                        if obj.get_dataclass()["x"] > 0:
+                            if obj.get_dataclass()["speedPoints"] >= step:
+                                obj.edit("x", obj.get_dataclass()["x"] - 1)
+                        else:
+                            entity.pop(entity.index(obj))
+                    collisionCheck(obj, player)
+
+            level = loadLevel(n, m)
+            updateLevel(level, entity)
+            printInterface(level, entity)
+            time.sleep(0.01)
+
+            if player.get_dataclass()["symbol"] == "#":
+                damageTaken(level, entity)
+
+        if random.randint(0, 5) != 0:
+            meteor = generateMeteor(n, m)
+            entity = [meteor] + entity
+
+        player.edit("scorePoints", player.get_dataclass()["scorePoints"] + 1)
+
+    if player.get_dataclass()["healthPoints"] <= 0:
+        gameOver(1, m)
+    else:
+        gameOver(0, m)
+
+
+if __name__ == "__main__":
+    complexity = "Легкая"
+
+    mainMenu(complexity)
+
+    while 1:
+        choice = msvcrt.getwch()
+        while choice not in "123":
+            choice = msvcrt.getwch()
+
+        if choice == "1":
+            main(21 if complexity == "Легкая" else (11 if complexity == "Нормальная" else 5))
+        elif choice == "2":
+            changeComplexity()
+
+            choice = msvcrt.getwch()
+            while choice not in "123":
+                choice = msvcrt.getwch()
+
+            if choice == "1":
+                complexity = "Легкая"
+            elif choice == "2":
+                complexity = "Нормальная"
             else:
-                self.y += self.speed
-                if self.y >= self.target[1]:
-                    self.y = self.target[1]
-                    self.move = False
+                complexity = "Сложная"
+        else:
+            sys.exit()
 
-
-def isWin(matrix, n):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] != n * n:
-                if matrix[i][j] != (i * n + j + 1):
-                    return False
-    return True
-
-
-def generateNumbers(matrix, n):
-    flags = [False for i in range(n * n)]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            matrix[i][j] = random.randint(1, n * n)
-            while flags[matrix[i][j] - 1]:
-                matrix[i][j] = random.randint(1, n * n)
-            flags[matrix[i][j] - 1] = True
-
-    return matrix
-
-
-GRAY = (100, 100, 100)
-WHITE = (255, 255, 255)
-RED = (180, 0, 0)
-
-tile_size = 100
-tile_speed = 25
-
-indent = 10
-
-n = 4
-W = tile_size * n + indent
-H = W + 50
-
-gameover = False
-
-fps = 60
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((W, H))
-pygame.display.set_caption("Пятнашки")
-clock = pygame.time.Clock()
-
-restart = False
-
-while 1:
-    if not gameover:
-        restart = False
-        numbers = [[0 for j in range(n)] for i in range(n)]
-
-        numbers = generateNumbers(numbers, n)
-
-        tiles = []
-
-        for i in range(n):
-            for j in range(n):
-                if numbers[i][j] == n * n:
-                    empty_tile_x = j * tile_size + indent
-                    empty_tile_y = i * tile_size + indent
-                    empty_tile_i = i
-                    empty_tile_j = j
-                else:
-                    tiles.append(
-                        Tile(numbers[i][j], j * tile_size + indent, i * tile_size + indent, tile_speed,
-                             tile_size - indent))
-
-    while (not gameover) and (not restart):
-        clock.tick(fps)
-
-        screen.fill(WHITE)
-
-        for tile in tiles:
-            if tile.number != n * n:
-                pygame.draw.rect(screen, GRAY,
-                                 (tile.x, tile.y, tile.size, tile.size))
-
-                font = pygame.font.Font(font_path, 50)
-                text = font.render(f"{tile.number}", True, WHITE)
-                screen.blit(text, [tile.x, tile.y])
-
-                font = pygame.font.Font(font_path, 40)
-                text = font.render("Esc - начать сначала", True, RED)
-                screen.blit(text, [25, H - 40])
-
-        pygame.display.update()
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                pos = event.pos
-
-                for i in range(len(tiles)):
-                    if (not tiles[i].move) and pos[0] > tiles[i].x and pos[1] > tiles[i].y and pos[0] < tiles[i].x + \
-                            tiles[i].size and pos[1] < tiles[i].y + tiles[i].size:
-                        if tiles[i].x + tile_size == empty_tile_x and tiles[i].y == empty_tile_y:
-                            tiles[i].direction = 2
-                            tiles[i].move = True
-
-                        elif tiles[i].x - tile_size == empty_tile_x and tiles[i].y == empty_tile_y:
-                            tiles[i].direction = 1
-                            tiles[i].move = True
-
-                        elif tiles[i].y + tile_size == empty_tile_y and tiles[i].x == empty_tile_x:
-                            tiles[i].direction = 3
-                            tiles[i].move = True
-
-                        elif tiles[i].y - tile_size == empty_tile_y and tiles[i].x == empty_tile_x:
-                            tiles[i].direction = 0
-                            tiles[i].move = True
-
-                        if tiles[i].move:
-                            tiles[i].target = [empty_tile_x, empty_tile_y]
-
-                            empty_tile_x = tiles[i].x
-                            empty_tile_y = tiles[i].y
-
-                            numbers[(tiles[i].x - indent) // tile_size][(tiles[i].y - indent) // tile_size], numbers[empty_tile_i][empty_tile_j] = numbers[empty_tile_i][empty_tile_j], numbers[(tiles[i].x - indent) // tile_size][(tiles[i].y - indent) // tile_size]
-
-                            empty_tile_i, empty_tile_j = (tiles[i].x - indent) // tile_size, (tiles[i].y - indent) // tile_size
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    restart = True
-
-        gameover = isWin(numbers, n)
-
-        for tile in tiles:
-            tile.Update()
-
-    if gameover:
-        screen.fill(WHITE)
-
-        font = pygame.font.Font(font_path, W // 10)
-        message = font.render('Вы победили!', True, RED, WHITE)
-        mes_rect = message.get_rect(center=(W / 2, H / 2))
-        screen.blit(message, mes_rect)
-
-        font = pygame.font.Font(font_path, W // 20)
-        message = font.render('Нажмите любую клавишу', True, RED, WHITE)
-        mes_rect = message.get_rect(center=(W / 2, H / 2 + H / 3))
-        screen.blit(message, mes_rect)
-
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                gameover = False
+        mainMenu(complexity)
